fc387666b7bea3c7a71a6fb67917006370fd4017
ravenusmc <mcuddy77@gmail.com>
2016-08-10 09:18:41 -0400

    wrote mainMenu function

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-#Main File for the project. 
+#Main File for the project.
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
 
+from valid import *
+ 
 def main():
+  print("\033c")
   print("Welcome to European Terrorism Data.")
   print("This program will allow the user to see the amount of people killed by terrorism.")
-  print("The data goes from 1970 to the present.")
+  print("The data has data from 1970 to the present.")
   print("1. Use the program")
   print("2. Quit")
   choice = int(input("What is your choice?(1/2) "))
@@ -16,9 +22,27 @@ def main():
     print("I hope that you come again!")
 
 def mainMenu():
+  print("\033c")
   print("Welcome to the main menu")
   print("1. Look at data")
   print("2. Quit")
+  choice = int(input("What is your choice? "))
+  while not validMain(choice):
+    choice = int(input("What is your choice? "))
+  if choice == 1:
+    data()
+  elif choice == 2:
+    print("Thank you for stopping by!")
+    print("I hope that you come again!")
+
+def data():
+  print("\033c")
+  print("Welcome to the data section")
+  print("Here you will decide how you want to look at the data")
+  deaths = pd.read_csv('eu_terrorism_fatalities.csv')
+  print("")
+
+
 
 
 main()

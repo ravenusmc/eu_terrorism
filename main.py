3374fd2d33e819bb3b01485b723a4d165225d2a2
ravenusmc <mcuddy77@gmail.com>
2016-08-10 09:40:56 -0400

    finished data function

--- main.py
+++ main.py
@@ -40,9 +40,66 @@ def data():
   print("Welcome to the data section")
   print("Here you will decide how you want to look at the data")
   deaths = pd.read_csv('eu_terrorism_fatalities.csv')
-  print("")
+  print("1. Individual Country")
+  print("2. Multiple Countries")
+  print("3. Main Menu")
+  print("4. Quit")
+  choice = int(input("What is your choice? "))
+  while not validData(choice):
+    choice = int(input("What is your choice? "))
+  if choice == 1:
+    singleCountry(deaths)
+  elif choice == 2:
+    multipleCountry(deaths)
+  elif choice == 3:
+    mainMenu()
+  elif choice ==  4:
+    print("Thank you for stopping by!")
+    print("I hope that you come again!")
 
+def singleCounty(deaths):
+  print("\033c")
+  print("Welcome to single country deaths")
+  print("Belgium")
+  print("Denmark")
+  print("3. France")
+  print("4. Germany")
+  print("5. Greece")
+  print("6. Ireland")
+  print("7. Italy")
+  print("8. Luxembourg")
+  print("9. Netherlands")
+  print("10. Portugal")
+  print("11. Spain")
+  print("12. United Kingdom")
+  choice = input 
 
 
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- valid.py
+++ valid.py
@@ -5,3 +5,9 @@ def validMain(choice):
     return True 
   else:
     return False 
+
+def validData(choice):
+  if choice == 1 or choice == 2 or choice == 3 or choice == 4:
+    return True
+  else:
+    return False
